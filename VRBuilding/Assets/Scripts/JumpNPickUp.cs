25812a588486bce85134a2efac0cf781a0f20d04
yunlei Zhang <yunlei Zhang>
2017-09-05 22:08:33 -0400

    add pickup function

---
+++ VRBuilding/Assets/Scripts/JumpNPickUp.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class JumpNPickUp : MonoBehaviour
+{
+    public const float jumpSpeedSet = 3;
+    public const float gravitySet = 9.8f;
+
+    SteamVR_TrackedObject trackedObj;
+    public CharacterController charController;
+    //public Movement movement;
+    public float jumpSpeed = jumpSpeedSet;
+    public float gravity = gravitySet;
+    public bool isButtonPressed;
+    SteamVR_Controller.Device device;
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+    }
+    // Use this for initialization
+    void Start()
+    {
+    }
+    public float GetJumpSpeedSet()
+    {
+        return jumpSpeedSet;
+    }
+    public float GetGravitySet()
+    {
+        return gravitySet;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        isButtonPressed = device.GetPress(SteamVR_Controller.ButtonMask.ButtonA);
+        //Debug.Log("isButtonPressed" + isButtonPressed);
+        //Debug.Log("isTriggerPressed" + device.GetPress(SteamVR_Controller.ButtonMask.Trigger));
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("PickUp"))
+        {
+            if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+            {
+
+            }
+        }
+
+    }
+}
---
+++ VRBuilding/Assets/Scripts/Pickup.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class Pickup : MonoBehaviour
+{
+
+    SteamVR_TrackedObject trackedObj;
+    public CharacterController charController;
+    SteamVR_Controller.Device device;
+    Transform originalParent;
+    //public Movement movement;
+    //public bool isButtonPressed;
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        //isButtonPressed = device.GetPress(SteamVR_Controller.ButtonMask.ButtonA);
+        //Debug.Log("isButtonPressed" + isButtonPressed);
+        //Debug.Log("isTriggerPressed" + device.GetPress(SteamVR_Controller.ButtonMask.Trigger));
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+       
+        if (other.gameObject.layer == LayerMask.NameToLayer("PickUp") && device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            other.attachedRigidbody.isKinematic = true;
+            other.attachedRigidbody.useGravity = false;
+            other.transform.position = transform.position;
+            other.transform.rotation = transform.rotation;
+            originalParent = other.gameObject.transform.parent;
+            other.gameObject.transform.SetParent(transform);
+        }
+        if(device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+
+            other.gameObject.transform.SetParent(originalParent);
+            other.attachedRigidbody.isKinematic = false;
+            other.attachedRigidbody.useGravity = true;
+            tossObject(other.attachedRigidbody);
+        }
+        
+    }
+
+    void tossObject(Rigidbody rigidbody)
+    {
+        Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+        if(origin != null)
+        {
+            rigidbody.velocity = origin.TransformVector(device.velocity);
+            rigidbody.angularVelocity = origin.TransformVector(device.angularVelocity);
+
+        }
+        else
+        {
+            rigidbody.velocity = device.velocity;
+            rigidbody.angularVelocity = device.angularVelocity;
+        }
+    }
+
+
+}
