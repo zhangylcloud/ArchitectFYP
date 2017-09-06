84bb520393a4884e682747f63d92e7cc964c05ec
yunlei Zhang <yunlei Zhang>
2017-09-06 00:55:58 -0400

    add water flower

---
+++ VRBuilding/Assets/Scripts/BottleShifter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleShifter : MonoBehaviour {
+    public GameObject bottle1;
+    public GameObject bottle2;
+    public bool isFull;
+	// Use this for initialization
+	void Start () {
+        isFull = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ShiftBottle()
+    {
+        bottle1.SetActive(false);
+        bottle2.SetActive(true);
+    }
+
+    
+
+    private void FixedUpdate()
+    {
+        if(Physics.Raycast(transform.position, new Vector3(0, -1, 0), 0.2f, 1 << LayerMask.NameToLayer("River")))
+        {
+            ShiftBottle();
+            isFull = true;
+        }
+        RaycastHit hitObj;
+        if (isFull && Physics.Raycast(transform.position, transform.TransformDirection(new Vector3(0, 1, 0)),out hitObj, 2.0f, 1 << LayerMask.NameToLayer("Flower")))
+        {
+            hitObj.collider.gameObject.GetComponent<GrassFlowerShifter>().Shift();
+        }
+    }
+}
---
+++ VRBuilding/Assets/Scripts/GrassFlowerShifter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassFlowerShifter : MonoBehaviour {
+    public GameObject grass;
+    public GameObject flower;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Shift()
+    {
+        grass.SetActive(false);
+        flower.SetActive(true);
+    }
+}
