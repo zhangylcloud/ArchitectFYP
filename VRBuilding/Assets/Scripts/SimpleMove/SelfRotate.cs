dc121dd9438408b0cac2c9e885ee0b1ff3959d4e
yunlei Zhang <yunlei Zhang>
2017-09-06 12:19:59 -0400

    add simple move

---
+++ VRBuilding/Assets/Scripts/SimpleMove/SelfRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotate : MonoBehaviour {
+    public float rotateSpeed;
+    public Transform point1;
+    public Transform point2;
+    private Vector3 axis;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        axis = point2.position - point1.position;
+        transform.RotateAround(point1.position, axis, rotateSpeed * Time.deltaTime);
+	}
+}
---
+++ VRBuilding/Assets/Scripts/SimpleMove/UpNDown.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpNDown : MonoBehaviour {
+    public float moveSpeed;
+    public float moveRange;
+    private float totalMove;
+    private bool upODown;
+    Vector3 speedVec;
+    public int dir;
+    private Vector3[] dirVecs = new Vector3[3];
+    
+	// Use this for initialization
+	void Start () {
+        
+        upODown = true;
+        totalMove = 0;
+        dirVecs[0] = new Vector3(0, 1, 0);
+        dirVecs[1] = new Vector3(1, 0, 0);
+        dirVecs[2] = new Vector3(0, 0, 1);
+    }
+	
+	// Update is called once per frame
+
+	void Update () {
+        speedVec = dirVecs[dir] * moveSpeed * Time.deltaTime;
+        if (upODown)
+        {
+            transform.Translate(speedVec);
+            totalMove += (dir == 0 ? speedVec.y : (dir == 1) ? speedVec.x : speedVec.z);
+            if (totalMove > moveRange)
+            {
+                upODown = !upODown;
+                totalMove = 0;
+            }
+        }
+        else
+        {
+            transform.Translate(-1 * speedVec);
+            totalMove -= (dir == 0 ? speedVec.y : (dir == 1) ? speedVec.x : speedVec.z);
+            if (totalMove < -moveRange)
+            {
+                upODown = !upODown;
+                totalMove = 0;
+            }
+        }
+	}
+}
